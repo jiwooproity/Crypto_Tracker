8a4e4a085dcae5ee023cf2d1d0cfa7c7875cf898
jiwooproity <jiwooproity@naver.com>
2022-01-02 20:59:33 +0900

    ohlcv 차트 추가

--- crypto_tracker/src/components/ChartList.tsx
+++ crypto_tracker/src/components/ChartList.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { fetchPriceHistory } from "../config/api";
+import ApexChart from "react-apexcharts";
 
 interface chartProps {
     coinId: string;
@@ -27,15 +28,78 @@ const PropH1 = styled.h1`
 
 function ChartList({coinId}: chartProps) {
     const { isLoading, data } = useQuery<IChartProps[]>(["ohlcvData", coinId], () => fetchPriceHistory(coinId));
+    
     return (
         <>
             {isLoading ? (
                 "loading..."
             ) : (
-                <>
-                    <PropH1>{coinId}</PropH1>
-                    <p>차트 준비중</p>
-                </>
+                <ApexChart 
+                    type="candlestick"
+
+                    series={[
+                        {
+                            name: "price",
+                            data: data?.map((price) => {
+                                return {
+                                    x: price.time_close,
+                                    y: [price.open.toFixed(2), price.high.toFixed(2), price.low.toFixed(2), price.close.toFixed(2)],
+                                }
+                            })
+                        }
+                    ]}
+
+                    options={{
+                        theme: {
+                            mode: "dark",
+                        },
+    
+                        chart: {
+                            width: "100%",
+                            toolbar: {
+                                show: false,
+                            },
+    
+                            background: "transparent",
+                        },
+    
+                        grid: {
+                            show: false,
+                        },
+    
+                        stroke: {
+                            curve: "smooth",
+                            width: 1,
+                        },
+    
+                        yaxis: {
+                            show: false,
+                        },
+    
+                        xaxis: {
+                            axisBorder: {
+                                show: false,
+                            },
+    
+                            axisTicks: {
+                                show: false,
+                            },
+    
+                            labels: {
+                                show: false,
+                            },
+    
+                            type: "datetime",
+                            categories: data?.map((price) => price.time_close),
+                        },
+    
+                        tooltip: {
+                            y: {
+                                formatter: (value) => `$ ${value.toFixed(3)}`,
+                            }
+                        }
+                    }}
+                />
             )}
         </>
     )
